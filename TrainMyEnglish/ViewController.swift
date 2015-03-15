9a92899eb5e158820f0548898049c7daa912c3d1
Stas Kirichok <stas1941@ua.fm>
2015-03-15 13:49:02 +0200

    Initial Commit

---
+++ TrainMyEnglish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainMyEnglish
+//
+//  Created by Stas Kirichok on 15.03.15.
+//  Copyright (c) 2015 Stas Kirichok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
